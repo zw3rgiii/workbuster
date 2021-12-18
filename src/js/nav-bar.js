c2c52b19777722a89eb1861d1b958278d3bf1f67
m-tarte <92918623+m-tarte@users.noreply.github.com>
2021-12-18 22:50:51 +0100

    Hamburger menu navbar fixed

--- src/js/nav-bar.js
+++ src/js/nav-bar.js
@@ -4,9 +4,12 @@ window.addEventListener('scroll', function(){
 })
         
 function enterHamburger(){
-    console.log("TEST");
-}
+    var nav = document.querySelector('.logo');
+    var menu = document.querySelector('.hamburger-menu');
 
-function leaveHamburger(){
-    
+    nav.classList.toggle("nav-hamburger-opened");
+    menu.classList.toggle("menu-hamburger-opened");
+
+    nav.classList.toggle("nav-hamburger-closed");
+    menu.classList.toggle("menu-hamburger-closed");
 }
